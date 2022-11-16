0f0ee196c8d336027d84d436d2a6141e71b29d64
Akhil Kosuri <64255484+akhil838@users.noreply.github.com>
2022-11-16 20:07:38 +0530

    Update Arr_int.c

--- Arr_int.c
+++ Arr_int.c
@@ -4,14 +4,15 @@ int main() {
    int n;
    printf("enter number of intergers: ");
    scanf("%d",&n);
-   int num[n];
-   int min,max,sum=0;
+   int num[n],min,max;
+   float sum=0;
    for (int i=0;i<n;i++){
        scanf("%d",&num[i]);
        sum += num[i];
        if (num[i]>num[i-1]){max=num[i];};
        if (num[i]<num[i-1]){min=num[i];};
    }
-   printf("MIN = %d\nMAX = %d\nSum of Integers = %d\n",min,max,sum);
+   float avg=sum/n;
+   printf("MIN = %d\nMAX = %d\nSum of Integers = %.2f\nAverage of Integers = %.2f\n",min,max,sum,avg);
     return 0;
 }
